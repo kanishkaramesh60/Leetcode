31d1aa1094953a77024fa811f55461ca95156fcf
Kanishka <kanishkaramesh60@gmail.com>
2026-06-27 19:59:42 +0530

    Added two pointer problem

---
+++ Two pointer/28 Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,31 @@
+"""28. Find the Index of the First Occurrence in a String
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1."""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        i=0
+        j=0
+        while i<len(haystack):
+            if haystack[i]==needle[j]:
+                i+=1
+                j+=1
+                if j==len(needle):
+                    return (i-j)
+            else:
+                i=i-j+1
+                j=0
+        return -1
